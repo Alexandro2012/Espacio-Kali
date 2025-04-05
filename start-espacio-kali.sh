91dc6521e56268b5e46c3f2cfa4b2f496c40490c
Alexandre2023 <latasbasias2@gmail.com>
2025-04-05 16:43:02 +0200

    Update start-espacio-kali.sh

--- start-espacio-kali.sh
+++ start-espacio-kali.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Cargar fondo de pantalla si lo deseas (opcional)
-feh --bg-scale ~/Espacio-Kali/wallpapers/wallpaper.png &
+# Fondo de pantalla
+feh --bg-scale $HOME/Espacio-Kali/wallpapers/wallpaper.png &
 
 # Iniciar Polybar
-~/.config/polybar/launch.sh &
+$HOME/.config/polybar/launch.sh &
 
-# Esperar un poco para asegurarse de que todo cargue
+# Espera para que se estabilice
 sleep 1
 
-# Abrir rofi en modo dock como menú principal (opcional)
-# rofi -show drun &
-
-# Mantener la sesión viva (usamos openbox para gestionar ventanas sin mostrar su panel)
+# Inicia gestor de ventanas (usa openbox, o el que prefieras)
 exec openbox
